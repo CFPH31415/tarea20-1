91e72774844586216d0e77675dca707110c9adad
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-17 10:32:43 -0500

    con estilos

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
+import { InspectorService } from './services/inspector.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PagesModule
+  ],
+  providers: [{provide:HTTP_INTERCEPTORS, useClass:InspectorService, multi:true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
